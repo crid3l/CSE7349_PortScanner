99c1cd726de97860ebc526e6d2cb646e41a3109e
Rye-Ton <tcriddle@smu.edu>
2019-05-05 15:16:23 -0500

    pcapfile test

--- src/pcapreader.py
+++ src/pcapreader.py
@@ -5,6 +5,10 @@ urls = ["connect_scan.pcapng,  multiplescans.pcapng,  scan.pcapng,  tcp_syn_scan
 
 def main():
     print("Hello World")
+    testcap = open(basePath + urls[0], 'rb')
+    capfile = savefile.load_savefile(testcap, verbose=True)
+    print (capfile)
+    return
 
 if __name__ == '__main__':
     main()
