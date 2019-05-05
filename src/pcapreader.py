81a5984c2147e32c5ac35a97726bff3837199df6
Rye-Ton <tcriddle@smu.edu>
2019-05-05 15:18:18 -0500

    fix string urls

--- src/pcapreader.py
+++ src/pcapreader.py
@@ -1,11 +1,11 @@
 from pcapfile import savefile
 
 basePath = "../../../../opt/scans/"
-urls = ["connect_scan.pcapng,  multiplescans.pcapng,  scan.pcapng,  tcp_syn_scan.pcapng"]
+paths = ["connect_scan.pcapng",  "multiplescans.pcapng",  "scan.pcapng",  "tcp_syn_scan.pcapng"]
 
 def main():
     print("Hello World")
-    testcap = open(basePath + urls[0], 'rb')
+    testcap = open(basePath + paths[0], 'rb')
     capfile = savefile.load_savefile(testcap, verbose=True)
     print (capfile)
     return
