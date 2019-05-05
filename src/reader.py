bbb4f0348cb9348076d0603462b5b0ea2679c80d
Rye-Ton <tcriddle@smu.edu>
2019-05-05 16:51:17 -0500

    bug fix

--- src/reader.py
+++ src/reader.py
@@ -4,7 +4,7 @@ basePath = "./../../../../opt/scans/"
 paths = ["connect_scan.pcapng",  "multiplescans.pcapng",  "scan.pcapng",  "tcp_syn_scan.pcapng"]
 
 # rdpcap comes from scapy and loads in our pcap file
-packets = rdpcap(basePath + paths)
+packets = rdpcap(basePath + paths[0])
 
 # Let's iterate through every packet
 for packet in packets:
