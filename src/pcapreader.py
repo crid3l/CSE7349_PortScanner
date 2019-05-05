609ac1deab52bccf5f81628fd9dff847e9ea489f
Rye-Ton <tcriddle@smu.edu>
2019-05-05 16:41:37 -0500

    push

--- src/pcapreader.py
+++ src/pcapreader.py
@@ -1,11 +1,18 @@
 from pcapfile import savefile
 
+debug = False
+debugPath = "../connect_scan.pcapng"
+
 basePath = "./../../../../opt/scans/"
 paths = ["connect_scan.pcapng",  "multiplescans.pcapng",  "scan.pcapng",  "tcp_syn_scan.pcapng"]
 
 def main():
     print("Hello World")
-    myCap = open(basePath + paths[0], 'rb')
+    if(debug):
+        myCap = open(debugPath)
+    else:
+        myCap = open(basePath + paths[0], 'rb')
+
     capfile = savefile.load_savefile(myCap, verbose=True)
     # print (capfile)
     # return
