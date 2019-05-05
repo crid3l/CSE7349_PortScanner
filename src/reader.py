362300f89e271bff06b85ab14503780f3e38978b
Rye-Ton <tcriddle@smu.edu>
2019-05-05 17:21:18 -0500

    summary read

--- src/reader.py
+++ src/reader.py
@@ -9,4 +9,4 @@ print("Loading Packets")
 # Let's iterate through every packet
 for packet in packets:
     # We're only interested packets with a DNS Round Robin layer
-    packet[2]
+    packet.summary()
