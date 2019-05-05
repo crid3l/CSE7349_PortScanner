56a525958392492afcae07fbae0e26a958e36b27
Muaz <muaz-4692682418@Muazs-MacBook-Pro.local>
2019-05-05 18:02:36 -0500

    sniffer working

---
+++ src/sniff.py
@@ -0,0 +1,21 @@
+from scapy.all import *
+
+
+from collections import Counter
+from scapy.all import sniff
+ 
+## Create a Packet Counter
+packet_counts = Counter()
+ 
+## Define our Custom Action function
+def custom_action(packet):
+   packet.show()
+   print ("end of packet ---------------")
+ 
+## Setup sniff, filtering for IP traffic
+sniff(filter="tcp", prn=custom_action)
+ 
+## Print out packet count per A <--> Z address pair
+print("done")
+ 
+ 
