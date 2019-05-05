aef52e4e3482c996492ff7e474d03d9792bf58a8
Rye-Ton <tcriddle@smu.edu>
2019-05-05 16:44:55 -0500

    read port

--- src/reader.py
+++ src/reader.py
@@ -4,12 +4,9 @@ basePath = "./../../../../opt/scans/"
 paths = ["connect_scan.pcapng",  "multiplescans.pcapng",  "scan.pcapng",  "tcp_syn_scan.pcapng"]
 
 # rdpcap comes from scapy and loads in our pcap file
+print("Loading Packets")
 packets = rdpcap(basePath + paths[0])
 
 # Let's iterate through every packet
 for packet in packets:
-    # We're only interested packets with a DNS Round Robin layer
-    if packet.haslayer(DNSRR):
-        # If the an(swer) is a DNSRR, print the name it replied with.
-        if isinstance(packet.an, DNSRR):
-            print(packet.an.rrname)
+    print(packet)
