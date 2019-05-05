8a77d9cd3fc55fb9b06582430c657f162ca194b3
Rye-Ton <tcriddle@smu.edu>
2019-05-05 16:50:42 -0500

    display files

--- src/reader.py
+++ src/reader.py
@@ -4,9 +4,9 @@ basePath = "./../../../../opt/scans/"
 paths = ["connect_scan.pcapng",  "multiplescans.pcapng",  "scan.pcapng",  "tcp_syn_scan.pcapng"]
 
 # rdpcap comes from scapy and loads in our pcap file
-print("Loading Packets")
-packets = rdpcap(basePath + paths[0])
+packets = rdpcap(basePath + paths)
 
 # Let's iterate through every packet
 for packet in packets:
-    print(packet)
+    # We're only interested packets with a DNS Round Robin layer
+    packet.show()
