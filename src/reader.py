ce50703dd02578e5815e6fb5a72beffd8b1d1eb9
Rye-Ton <tcriddle@smu.edu>
2019-05-05 17:35:53 -0500

    reading packets

--- src/reader.py
+++ src/reader.py
@@ -9,4 +9,9 @@ print("Loading Packets")
 # Let's iterate through every packet
 for packet in packets:
     # We're only interested packets with a DNS Round Robin layer
+    TCP = packet['TCP']
+    print(TCP)
+    # print(TCP.sport)
+    # print(TCP.dport)
     print(packet['TCP'].sport)
+    print(packet['TCP'])
