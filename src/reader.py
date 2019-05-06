078750ae9dc219fe33b434b11d5a736bcc6a6fb0
Rye-Ton <tcriddle@smu.edu>
2019-05-06 16:59:29 -0500

    source

--- src/reader.py
+++ src/reader.py
@@ -36,9 +36,9 @@ for packet in packets:
         # check if address is local
         if IP.dst == "129.119.201.21":
             # check if current packet TCP field exist in sourceport list
-            if IP.src in sourcePorts:
+            if TCP.sport in sourcePorts:
                 # if so, we wan to update the end field
-                port = sourcePorts[IP.src]
+                port = sourcePorts[TCP.port]
                 port['end'] = timeStamp[1]
                 
                 #and add or increment a new destination.
@@ -46,7 +46,7 @@ for packet in packets:
                     port['dst'][TCP.dport] = port['dst'][TCP.dport] + 1
                 else:
                     port['dst'][TCP.dport] = 1
-                sourcePorts[IP.src] = port
+                sourcePorts[TCP.spot] = port
             # or initialize a new  port in the list
             else:
                 x = {
@@ -56,12 +56,11 @@ for packet in packets:
                         TCP.dport : 1
                     }
                 }
-                sourcePorts[IP.src] = x
+                sourcePorts[TCP.sport] = x
     except IndexError:
         # packet.show()
         continue
     # print("$$$$$*****PACKET-END*****$$$$$")
-    print("")
 print(destinationPorts)
 for key, val in sourcePorts.items():
     print(key)
